42211d4e19b23c736e1ac52ff8fba63380f32a46
JosepRIvaille <josep.de.cid@gmail.com>
2018-05-25 23:38:58 +0200

    First version parser

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableData: '',
+      numberCols: 4,
+      latexData: ''
+    };
+  }
+
+  onColumnsChange(numberCols) {
+    this.setState({ numberCols });
+  }
+
+  onDataChange(tableData) {
+    this.setState({ tableData });
+  }
+
+  convertToChunckedData(data) {
+    const { numberCols } = this.state;
+    let columnsLeft = numberCols;
+    return data.reduce((acc, x) => {
+      if (columnsLeft === 0) {
+        columnsLeft = numberCols - 1;
+        acc.push([x]);
+      } else {
+        --columnsLeft;
+        acc[acc.length - 1].push(x);
+      } return acc;
+    }, [[]]);
+  }
+
+  convertToLatex(data) {
+    let result = '';
+    data.forEach(row => {
+      row.forEach(value => { result += `${value} & ` });
+      result = result.slice(0, -2);
+      result += '\\\\ \\hline\n';
+    }); return result.slice(0, -1);
+  }
+
+  onGenerateOutputTable() {
+    const { tableData, numberCols } = this.state;
+    const splittedData = tableData.trim().split(/\s+/);
+    const chunckedData = this.convertToChunckedData(splittedData);
+    const latexData = this.convertToLatex(chunckedData);
+    this.setState({ latexData });
+  }
+
   render() {
     return (
       <div>
@@ -11,34 +59,40 @@ export default class App extends Component {
             </a>
           </div>
         </nav>
-        <div className="container" >
 
+        <div className="container" >
           <h2 className="app-title">LatexTablePaste</h2>
           <br />
           <p>Submit your shitty table to instantly have it for your Latex assignment!</p>
 
           <div class="field input-table">
             <div class="control ">
-              <textarea className="textarea is-primary" type="text" placeholder="Paste your table here..."></textarea>
+              <textarea
+                className="textarea is-primary" type="text" placeholder="Paste your table here..."
+                value={this.state.tableData} onChange={event => this.onDataChange(event.target.value)}></textarea>
             </div>
           </div>
 
-          <div class="field">
-            <div class="control">
-              <button class="button is-rounded is-primary">Convert!</button>
-            </div>
-          </div>
-
-
-          <h4 className="app-title">Result</h4>
-          <div className="field result-table">
+          <div className="field">
             <div className="control">
-              <textarea className="textarea is-primary" type="text" placeholder="Latex result here..."></textarea>
+              <button
+                className="button is-rounded is-primary"
+                onClick={() => this.onGenerateOutputTable()}>Convert!
+            </button>
+            </div>
+
+            <h4 className="app-title">Result</h4>
+            <div className="field result-table">
+              <div className="control">
+                <textarea
+                  className="textarea is-primary"
+                  type="text" placeholder="Latex result here..."
+                  value={this.state.latexData}></textarea>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
     );
   }
 }

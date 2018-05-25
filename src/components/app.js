70f4c3ab9fc0d3eef2aff429d795a233042a2b2e
Gonzalo Recio <gonzalo.recio@est.fib.upc.edu>
2018-05-25 23:44:39 +0200

    Fix style

--- src/components/app.js
+++ src/components/app.js
@@ -5,42 +5,40 @@ export default class App extends Component {
     return (
       <div>
         <nav className="navbar is-primary" color="blue" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
+          <div className="navbar-brand">
             <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-              LatexTablePaste
+              <p>LatexTablePaste</p>
             </a>
-        </div>
-      </nav>
+          </div>
+        </nav>
+        <div className="container" >
 
-      <h2 className="app-title">LatexTablePaste</h2>
-      <br/>
-      <p>Submit your shitty table to instantly have it for your Latex assignment!</p>
+          <h2 className="app-title">LatexTablePaste</h2>
+          <br />
+          <p>Submit your shitty table to instantly have it for your Latex assignment!</p>
 
-        <div class="field input-table">
-          <div class="control ">
-            <textarea className="textarea is-primary" type="text" placeholder="Paste your table here..."></textarea>
+          <div class="field input-table">
+            <div class="control ">
+              <textarea className="textarea is-primary" type="text" placeholder="Paste your table here..."></textarea>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <button class="button is-rounded is-primary">Convert!</button>
+            </div>
+          </div>
+
+
+          <h4 className="app-title">Result</h4>
+          <div className="field result-table">
+            <div className="control">
+              <textarea className="textarea is-primary" type="text" placeholder="Latex result here..."></textarea>
+            </div>
           </div>
         </div>
-
-        {/* <a className="button is-rounded">Submit!</a> */}
-        <div class="field">
-          <div class="control">
-            <button class="button is-rounded is-primary">Convert!</button>
-          </div>
-        </div>
-
-
-
-
-        <h4 className="app-title">Result</h4>
-        <div class="field result-table">
-          <div class="control">
-            <textarea className="textarea is-primary" type="text" placeholder="Latex result here..."></textarea>
-          </div>
-        </div>
-
       </div>
-     
+
     );
   }
 }
